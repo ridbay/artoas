0a0b2935707aa44ace4fd97b32a180967a5f785a
Ridwan Balogun <balogunridwan@gmail.com>
2020-08-25 13:59:20 +0100

    Added Contact form handler

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
+    $email_to = "ridwan@artoasglobal.com";
+    $email_subject = "New form submissions from the website";
 
     function problem($error)
     {
